02630c529ce592d2e737ebb5578ae45581ec0fe7
Magesh Kuppan <tkmagesh77@gmail.com>
2020-07-02 10:39:16 +0530

    function programming - assignment solutions

--- 01-Adv-Js/products.js
+++ 01-Adv-Js/products.js
@@ -7,11 +7,28 @@ var products = [
 ]
 
 //find the stationary products (use the 'filter' method)
+var stationaryProducts = products.filter(function(product){
+    return product.category === 'stationary'
+});
 
 //sort the products by cost (use the 'sort' method)
+products.sort(function(p1, p2){
+    return p1.cost - p2.cost
+});
 
 //find the maximum cost from the products list (use the reduce method)
+var maxCost = products.reduce(function(p1, p2){
+    return p1.cost > p2.cost ? p1.cost : p2.cost;
+});
 
 //create a new products array with the cost reduced by 10% (use the map method)
+var discountedProducts = products.map(function(product){
+    return {
+        id : product.id, name : product.name, cost : product.cost * 0.9, units : product.units, category : product.category
+    };
+});
 
 //print the 'name' and 'units' of all the products without using the for/while loop (use the forEach method)
+products.forEach(function(product){
+    console.log(product.name, product.units);
+});
