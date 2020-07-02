982ada5b2d54676133faaceee3f2e724c36c8138
Magesh Kuppan <tkmagesh77@gmail.com>
2020-07-02 10:18:13 +0530

    Prototypal Inheritance

--- 01-Adv-Js/Spinner(ConstructorFunction).js
+++ 01-Adv-Js/Spinner(ConstructorFunction).js
@@ -1,3 +1,6 @@
+//Using closures
+//    + privacy
+//    - larger memory footprint
 function Spinner(){
     var counter = 0;
     this.up = function up(){
@@ -6,5 +9,20 @@ function Spinner(){
     this.down = function down(){
         return --counter;
     };
+}
 
+//Using the prototype object
+//  + memory efficient
+//  - lack of privacy (use a convention to enforce privacy)
+
+function Spinner(){
+    this.__counter__ = 0;
+}
+
+Spinner.prototype.up = function(){
+    return ++this.__counter__;
+}
+
+Spinner.prototype.down = function(){
+    return --this.__counter__;
 }
