e13bfcf68b8e1b6a0f74a86f360cefed3d6c29dc
Magesh Kuppan <tkmagesh77@gmail.com>
2020-07-03 15:12:02 +0530

    calculator - assignment - solution

--- first-app/src/app/calculator/calculator.component.ts
+++ first-app/src/app/calculator/calculator.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
     styleUrls : ['./calculator.component.css']
 })
 export class CalculatorComponent{
-    result : number = 100;
+    result : number = 0;
+    n1 : number = 0;
+    n2 : number = 0;
+
+    setN1(value : number){
+        this.n1 = value;
+    }
+
+    setN2(value : number){
+        this.n2 = value;
+    }
+
+    onAddClick(){
+        this.result = this.n1 + this.n2;
+    }
+
+    onSubtractClick(){
+        this.result = this.n1 - this.n2;
+    }
+
+    onMultiplyClick(){
+        this.result = this.n1 * this.n2;
+    }
+
+    onDivideClick(){
+        this.result = this.n1 / this.n2;
+    }
 }
