f17bf642c23541aa2e48a842f1b378392155c4c5
Magesh Kuppan <tkmagesh77@gmail.com>
2020-07-06 16:28:23 +0530

    implementing 'TrimTextPipe' (custom pipe)

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from "./bugTracker/bugTracker.component";
+
+import { TrimTextPipe } from './bugTracker/pipes/trimText.pipe';
+
 @NgModule({
   declarations: [
     AppComponent
     , BugTrackerComponent
+    , TrimTextPipe
   ],
   imports: [
     BrowserModule
