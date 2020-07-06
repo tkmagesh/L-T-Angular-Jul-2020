835da8f46ceea81d4d602a9af81938aafd0efe9a
Magesh Kuppan <tkmagesh77@gmail.com>
2020-07-06 10:02:30 +0530

    Calculator - assignment -solution

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from "./calculator/calculator.component";
 import { SalaryCalculatorComponent } from './salaryCalculator/salaryCalculator.component';
-
+import { CalculatorTwoComponent } from './calculator/calculatorTwo.component';
 /* 
 declarations - UI entities of the application (Components, Pipes, Directives)
 providers - NON UI entities of the application (Services)
@@ -17,6 +17,7 @@ bootstrap - root component(s)
   declarations: [
     AppComponent
     , CalculatorComponent
+    , CalculatorTwoComponent
     , SalaryCalculatorComponent
   ],
   imports: [
@@ -27,6 +28,7 @@ bootstrap - root component(s)
     AppComponent
     , CalculatorComponent
     , SalaryCalculatorComponent
+    , CalculatorTwoComponent
   ]
 })
 export class AppModule { }
--- first-app/src/app/calculator/calculatorTwo.component.ts
+++ first-app/src/app/calculator/calculatorTwo.component.ts
@@ -1,1 +1,33 @@
-/* fill */
+import { Component } from "@angular/core";
+import { CalculatorModel } from "./calculatorModel";
+
+@Component({
+    selector : 'app-calculator-2',
+    templateUrl: './calculatorTwo.component.html'
+})
+export class CalculatorTwoComponent{
+    model : CalculatorModel = new CalculatorModel();
+
+    selectedOperator : string = '';
+
+    onCalculateClick() {
+        if (this.selectedOperator !== '')
+            this.model[this.selectedOperator]();
+        /* switch (this.selectedOperator) {
+            case 'add':
+                this.model.add();
+                break;
+            case 'subtract':
+                this.model.subtract();
+                break;
+            case 'multiply':
+                this.model.multiply();
+                break;
+            case 'divide':
+                this.model.divide();
+                break;
+            default:
+                break;
+        } */
+    }
+}
