513863e427dccbfe352b5908447603c98e192957
Magesh Kuppan <tkmagesh77@gmail.com>
2020-07-07 16:57:29 +0530

    refactoring to the Utils Module

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -1,32 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UtilsModule } from "../utils/utils.module";
 
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from "./bugTracker/bugTracker.component";
 import { BugStatsComponent } from "./bugTracker/views/bugStats.component";
 import { BugEditComponent } from "./bugTracker/views/bugEdit.component";
 
-import { TrimTextPipe } from './bugTracker/pipes/trimText.pipe';
-import { ElapsedPipe } from "./bugTracker/pipes/elapsed.pipe";
-import { SortPipe } from "./bugTracker/pipes/sort.pipe";
-
 import { ClosedCountPipe } from "./bugTracker/pipes/closedCount.pipe";
 import { BugOperationsService } from "./bugTracker/services/bugOperations.service";
 import { BugStorageService } from "./bugTracker/services/bugStorage.service";
 
 @NgModule({
-  declarations: [
+  declarations: [ 
     AppComponent
     , BugTrackerComponent
     , BugStatsComponent
     , BugEditComponent
     , ClosedCountPipe
-    , TrimTextPipe
-    , ElapsedPipe
-    , SortPipe
   ],
   imports: [
     BrowserModule
+    , UtilsModule
   ],
   providers: [
     BugOperationsService
